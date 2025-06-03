d45459581dc7ac6c7998f5683d9790b8b0c0c625
aladin01011 <alae01lakhdar@gmail.com>
2025-06-03 16:25:17 +0100

    Add basic routing: home and about pages with header links

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,11 +1,18 @@
 // components/layout/Header.tsx
-
+import Link from 'next/link';
 import React from 'react';
 
 const Header: React.FC = () => {
   return (
     <header className="bg-blue-500 p-4 text-white">
       <h1>Welcome to My Next.js Project</h1>
+      <nav>
+        <ul>
+          <li><Link href="/">Home (Root)</Link></li>
+          <li><Link href="/home">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+        </ul>
+      </nav>
     </header>
   );
 };

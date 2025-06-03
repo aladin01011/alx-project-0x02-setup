fcca9669da470d700597cc32a86df35a83cc187c
aladin01011 <alae01lakhdar@gmail.com>
2025-06-03 11:53:36 +0100

    Add Header component

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,13 @@
+// components/layout/Header.tsx
+
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-blue-500 p-4 text-white">
+      <h1>Welcome to My Next.js Project</h1>
+    </header>
+  );
+};
+
+export default Header;

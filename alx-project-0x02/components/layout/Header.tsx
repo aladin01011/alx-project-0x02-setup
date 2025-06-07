441b21f63288d5b4199a8046ca7996e95cdecffb
aladin01011 <alae01lakhdar@gmail.com>
2025-06-07 05:02:47 +0100

    Implement reusable Button component and Header navigation for /home, /about, /posts pages

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -11,6 +11,7 @@ const Header: React.FC = () => {
           <li><Link href="/">Home (Root)</Link></li>
           <li><Link href="/home">Home</Link></li>
           <li><Link href="/about">About</Link></li>
+          <li><Link href="/posts">Posts</Link></li>
         </ul>
       </nav>
     </header>
